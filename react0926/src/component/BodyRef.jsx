aec11fdf04542a804f5aaebffeb16c02b9be934b
Parkyounh <dkrfud9@naver.com>
2025-09-26 19:11:57 +0900

    react Ref

---
+++ react0926/src/component/BodyRef.jsx
@@ -0,0 +1,23 @@
+import { useState,useRef } from "react";
+
+function Body() {
+    const [text,setText] = useState("");
+    const inputRef = useRef(null);
+
+    const handleOnClick = (e) => {
+        const value = inputRef.current.value;
+        setText(value);
+    }
+
+
+    return (
+        <>
+        <h1>This is Body Component</h1>
+        <input type="text" ref={inputRef}/>
+        <p>{text}</p>
+        <button onClick={handleOnClick}>클릭</button>
+        </>
+    );
+}
+
+export default Body;
